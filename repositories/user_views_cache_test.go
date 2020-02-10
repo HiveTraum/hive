84024147fab7084384b79dacc96cf89c65979e84
Bergutov Ruslan <ruslanbergutov@gmail.com>
2020-02-10 19:28:34 +0500

    user view cache test

---
+++ repositories/user_views_cache_test.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"auth/config"
+	"auth/models"
+	"context"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetUserViewFromCache(t *testing.T) {
+	cache := config.InitRedis()
+	cache.FlushAll()
+	ctx := context.Background()
+	userView := &models.UserView{
+		Id:      uuid.NewV4(),
+		Created: 1,
+		Roles:   []string{"role"},
+		Phones:  []string{"phone"},
+		Emails:  []string{"email"},
+		RolesID: []uuid.UUID{uuid.NewV4()},
+	}
+	CacheUserView(cache, ctx, []*models.UserView{userView})
+	userViewFromCache := GetUserViewFromCache(cache, ctx, userView.Id)
+	require.Equal(t, userView, userViewFromCache)
+}
+
+func TestGetUserViewFromCacheWithoutUserView(t *testing.T) {
+	cache := config.InitRedis()
+	cache.FlushAll()
+	ctx := context.Background()
+	userViewFromCache := GetUserViewFromCache(cache, ctx, uuid.NewV4())
+	require.Nil(t, userViewFromCache)
+}
